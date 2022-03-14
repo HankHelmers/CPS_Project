13cbd215777762fa59d3fdc37818a686bcd1a4de
HankHelmers <89431423+HankHelmers@users.noreply.github.com>
2022-03-14 15:22:05 -0400

    Update Handle

--- Spotify API Retrieval/Spotify_Handler.m
+++ Spotify API Retrieval/Spotify_Handler.m
@@ -1,8 +1,12 @@
 %% CREATING A SAMPLE APP USING MATLAB APP DESIGNER
 
+%% Spotify_Handler class
+% Contols all interations with Spotify's API, where the UI can draw the
+% information necessary from this class to populate the visuals for the
+% user. 
 classdef Spotify_Handler 
     properties
-        username;
+        username; % spotify user's username
     end
 
     methods (Access = public)   
@@ -12,11 +16,9 @@ classdef Spotify_Handler
              obj.username = usernameInput; 
         end
 
-
+            
     end
 end
 
 
 
-
-%%
