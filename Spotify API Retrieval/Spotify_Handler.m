900ceaeaf13f9a59ba9d215bd526cf1446571b7d
HankHelmers <89431423+HankHelmers@users.noreply.github.com>
2022-03-25 13:11:01 -0500

    Update Testing

--- Spotify API Retrieval/Spotify_Handler.m
+++ Spotify API Retrieval/Spotify_Handler.m
@@ -14,8 +14,9 @@ classdef Spotify_Handler
         %   Input: Takes & sets a string for the user's username. 
         function obj = Spotify_Handler(usernameInput)
              obj.username = usernameInput; 
+             % retrieve info from spotify
+             % hihi 
         end
-
             
     end
 end
