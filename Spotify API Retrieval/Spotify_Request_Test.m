3c3c8c0ee0a1c6a7bc2cf8fa200b3533b0730f0b
HankHelmers <89431423+HankHelmers@users.noreply.github.com>
2022-03-28 12:10:29 -0500

    Spotify_Data_Retrievial

---
+++ Spotify API Retrieval/Spotify_Request_Test.m
@@ -0,0 +1,22 @@
+% Connecting Application to Spotify API & Making request. 
+
+client_id = "28ca2d3814414c08858c196d669a3838"; % Specific Application client id
+redirect_uri = 'http://localhost/';
+scope = 'user-read-private user-read-email';
+response_type = 'code';
+
+% spotify_url = "http://GET /playlist/?id=37i9dQZF1DX4Wsb4d7NKfP HTTP/1.1"
+% spotify_url = strcat(spotify_url, "X-Rapidapi-Host: spotify23.p.rapidapi.com")
+% spotify_url = strcat(spotify_url, "X-Rapidapi-Key: 91a5de44e4mshd40382508214f43p1fc36djsn3e75d9e497ec")
+% spotify_url = strcat(spotify_url, "Host: spotify23.p.rapidapi.com")
+
+% Authorization url
+auth_url = "https://accounts.spotify.com/authorize?";
+auth_url = strcat(auth_url, "client_id="+client_id);
+auth_url = strcat(auth_url, "&redirect_uri="+redirect_uri);
+auth_url = strcat(auth_url, "&response_type="+response_type);
+
+% retrieve from api
+[stat,h,url] = web(auth_url, '-browser');
+disp(url);
+%http://localhost/?code=AQCzCu4w_egGRDoHhYSEai1_lVIm2amdEeuw0zyDKiVVy8PDvkDSJ7AMqG-0ii_dgn0GhGkfd3p3F_g2uvq_TZWh72mMWCpn6cPlxOtrzv7lKRyqMVhQVOSUI7lvZWQdrcYMy0O0fLuWNMzyVjoBd37_TGwj
