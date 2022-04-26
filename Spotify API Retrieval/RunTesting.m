d3c45a0cca4fc9c4235ee7b0d68ff5bfe415216a
HankHelmers <89431423+HankHelmers@users.noreply.github.com>
2022-04-26 11:33:13 -0500

    Updating Results Functions

--- Spotify API Retrieval/RunTesting.m
+++ Spotify API Retrieval/RunTesting.m
@@ -2,14 +2,19 @@
 % THE PROGRAM.
 % WILL BE DELETED AND UNNESSESSARY FOR THE FILE PRODUCT. 
 
-[file,path] = uigetfile('*.csv')  %open a mat file
-filename = fullfile(path, file)
+[file,path] = uigetfile('*.csv');  %open a mat file
+filename = fullfile(path, file);
 data = readtable(filename);
-%SpotifyHandler.plotEnergyValence(data);
+data.valence = SpotifyHandler.normToRange(data.valence, 0, 1, -1, 1);
+data.energy = SpotifyHandler.normToRange(data.energy, 0, 1, -1, 1);
 
-SpotifyHandler.getHappySongs(data);
+SpotifyHandler.plotEnergyValence(data);
 
-%B = afterEach(A,fcn,n) 
-%SpotifyAuthorization.RequestAuthInAPI();
+% handle if they don't input the correct file ***8
 
-%py.SpotifyInteract.getTokenRequest('AQBkEyHUp21Z-oh1p9-RdoZzeKR_qRNhYOV6qPc_IugNicsg0KkTgFubnpJYwujOpVX-oJb9YBAXOa8RQr9-HYpFOai_8NfcmQFX1FByHeT7qp3C1MdJbgRSWL6_LpC6CrndjbMolXo1VO-6snkqCi7cHGgBsLI7_6-3ixR0rDUbKRUcumdLExvk9a4WlQ')
+% disp('High Energy & High Valence')
+% SpotifyHandler.getHappySongs();
+% 
+% disp('Low Energy & Low Valence')
+% SpotifyHandler.getSadSongs();
+% 
